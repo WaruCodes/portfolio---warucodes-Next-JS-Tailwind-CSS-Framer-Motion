6ae7a5869219a5f71dc504e946e71c80b62fa6f4
WaruCodes <sdrwarushika@std.appsc.sab.ac.lk>
2025-06-09 20:48:24 +0530

    Code on thilwindCss file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    darkMode: ["class"],
     content: [
       "./pages/**/*.{js,ts,jsx,tsx}",
       "./components/**/*.{js,ts,jsx,tsx}",
       "./app/**/*.{js,ts,jsx,tsx}",
+      "./src/**/*.{js,ts,jsx,tsx}",
     ],
+    prefix: "",
     theme: {
-      extend: {},
+        container: {
+            center: true,
+            padding: "15px",
+        },
+            screens: {
+                
+            },
+        },
+      extend: {
+        keyfreames: {
+            "accordian-down": {
+                from: {height: "0"},
+                to: {height: "var(--radix-accordian-content-height)"},
+            },
+            "accordian-down": {
+                from: {height: "var(--radix-accordian-content-height)"},
+                to: {height: "0"}, 
+            },
+        },
+      },
     },
     plugins: [],
   };
